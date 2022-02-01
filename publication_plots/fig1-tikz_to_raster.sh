8957890c08a58ad83e3f8d57a8cc87246c46b981
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2022-02-01 17:20:55 +0100

    use lzw compression for Fig1 TIF file

--- publication_plots/fig1-tikz_to_raster.sh
+++ publication_plots/fig1-tikz_to_raster.sh
@@ -1,4 +1,4 @@
 set -eu
 pdfcrop fig1-tikz.pdf fig1-tikz.pdf
 convert -flatten -density 300 fig1-tikz.pdf fig1-tikz.png
-convert -flatten -density 600 fig1-tikz.pdf fig1-tikz.tif
+convert -flatten -density 600 -compress lzw -units pixelsperinch -depth 8 fig1-tikz.pdf fig1-tikz.tif
